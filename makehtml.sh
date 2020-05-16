2f8faf0273170433b0d397a06912d3de97fe1d3e
gzhao4 <george.zhao@nike.com>
2020-05-16 05:08:41 +0000

    add hope

--- makehtml.sh
+++ makehtml.sh
@@ -1,2 +1,2 @@
-jupyter nbconvert --to notebook --inplace --execute find_confirmed.ipynb
+jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1800 --execute find_confirmed.ipynb
 jupyter nbconvert find_confirmed.ipynb --no-input
